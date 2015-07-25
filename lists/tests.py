518032c45d3d98e5a660a308be6da29e81245e77
beck <bbeck410@gmail.com>
2015-07-25 11:47:54 -0500

    homepage view returns hardcoded response

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,8 @@
 # resolve(URL).func maps the url to a view
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -10,3 +12,10 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_hoe_page_returns_correct_html(self):
+        request = HttpRequest()
+        response =home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))  #3
+        self.assertIn(b'<title>To-Do lists</title>', response.content)  #4
+        self.assertTrue(response.content.endswith(b'</html>'))
