15963b06706a37f14caaac01fbab26f1dbfc0446
beck <bbeck410@gmail.com>
2015-08-23 19:24:29 -0500

    added model with passing tests

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,9 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
+from lists.models import Item
 from lists.views import home_page
+
 
 # Create your tests here.
 class SmokeTest(TestCase):
@@ -16,6 +18,40 @@ class SmokeTest(TestCase):
 
     def test_hoe_page_returns_correct_html(self):
         request = HttpRequest()
-        response =home_page(request)
+        response = home_page(request)
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'a new list item'
+
+        response = home_page(request)
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'a new list item'}
+        )
+
+        self.assertIn('a new list item', response.content.decode())
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
